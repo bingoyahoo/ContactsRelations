8875de394332123d3e891628251beb7d5f4737d4
Delvin Low <delvinlowzy@gmail.com>
2015-05-28 00:28:54 +0800

    Add dummy data

--- app/src/main/java/com/delvinlow/android/contactsrelations/app/MainActivityFragment.java
+++ app/src/main/java/com/delvinlow/android/contactsrelations/app/MainActivityFragment.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    ArrayAdapter<String> mContactsAdapter;
 
     public MainActivityFragment() {
     }
@@ -18,6 +25,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        String[] data = {"Anna 1234567", "Belle 1234567", "Christina 1234567",
+                "Dawn 1234567", "Elaine 1234567", "Fiona 1234567", "Germaine 1234567"};
+        List<String> contacts = new ArrayList<String>(Arrays.asList(data));
+
+        //Creating the adapter
+        mContactsAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_contact,
+                R.id.list_item_contact_textview,
+                contacts);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_contact);
+        listView.setAdapter(mContactsAdapter);
+
+        return rootView;
+
     }
 }
